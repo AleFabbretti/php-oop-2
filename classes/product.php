75efe38915928f014ccd7a5cd01db52563be42cb
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-10 17:26:54 +0100

    Aggiunta classe product

---
+++ classes/product.php
@@ -0,0 +1,18 @@
+<?php 
+    class Product{
+        public $category;
+        public $image;
+        public $name;
+        public $price;
+        public $type;
+
+        public function __construct($_category, $_image, $_name, $_price, $_type){
+            $this->category = $_category;
+            $this->image = $_image;
+            $this->name = $_name;
+            $this->price = $_price;
+            $this->type = $_type;
+        }
+    }
+
+?>
