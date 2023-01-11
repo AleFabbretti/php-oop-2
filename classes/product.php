637694acae89cd0309e54f3c0c57dfc5aa514fc4
Alelisk91 <alessandrofabbretti91@gmail.com>
2023-01-11 16:46:05 +0100

    Aggiunto trait

--- classes/product.php
+++ classes/product.php
@@ -1,12 +1,14 @@
 <?php 
+    require_once __DIR__ . "/../traits/name.php";
     class Product{
+        use name;
         public $category;
         public $image;
         public $name;
         public $price;
         public $type;
 
-        public function __construct($_category, $_image, $_name, float $_price, $_type){
+        public function __construct(string $_category, string $_image, string $_name, float $_price, string $_type){
             $this->category = $_category;
             $this->image = $_image;
             $this->name = $_name;
